dffa3d02849448ef11e07fcc5f240bd9d01214a1
ABukSwienty <alexanderbukswienty@gmail.com>
2022-09-26 22:04:58 +0200

    fix(index): css

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="w-screen h-screen bg-orange-400 flex items-center justify-center">
+    <div className="w-screen h-screen bg-orange-600 flex items-center justify-center">
       <h1 className="text-3xl text-white">Coming soon...</h1>
     </div>
   );
