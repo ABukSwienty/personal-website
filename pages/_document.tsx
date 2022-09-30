6b2f30e0880cf226fdda149687df76de0ec012a3
ABukSwienty <alexanderbukswienty@gmail.com>
2022-09-30 11:10:30 +0200

    fix(favicon)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Arsenal:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           ></link>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👾</text></svg>"
+          />
         </Head>
         <body className="overflow-x-hidden bg-indigo-50 font-sans transition-colors duration-300 ease-in-out">
           <div id="overlay" className="relative z-50"></div>
